b3b5144642c3273661d83cf7e221d070d35b3d81
ZZBoxZZz <huydang1609.dpmh@gmail.com>
2023-01-12 21:28:49 +0700

    second update

--- react-form/src/components/BaiTapReactForm/FormSinhVien.jsx
+++ react-form/src/components/BaiTapReactForm/FormSinhVien.jsx
@@ -1,11 +1,88 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
-export default class FormSinhVien extends Component {
+class FormSinhVien extends Component {
+
+
+
+
+  state = {
+    maSV: '',
+    hoTen: '',
+    soDienThoai: '',
+    email: '',
+  }
+
+  handleChange = (e) => {
+    let tagInput = e.target;
+    let {name,value} = tagInput;
+  
+
+    this.setState({
+      [name]:value
+    },()=>{
+      console.log(this.state);
+    })
+  };
+
+
+  handleSubmit = (e) =>{
+    e.preventDefault();
+    this.props.themSinhVien(this.state);
+  }
   render() {
     return (
-      <div>
+      <div className='container'>
+
+
+        <div className="card text-left">
+          <div className='card-header bg-dark text-white'><h3>Thông tin SV</h3></div>
+          <div className="card-body">
+            <form onSubmit={this.handleSubmit}>
+              <div className='row'>
+                <div className='form-group col-6'>
+                  <span>Ma SV</span>
+                  <input type='text' className='form-control' name="maSV" value={this.state.maSV} onChange={this.handleChange} />
+                </div>
+                <div className='form-group col-6'>
+                  <span>Ho va Ten</span>
+                  <input type='text' className='form-control' name="hoTen" value={this.state.hoTen} onChange={this.handleChange} />
+                </div>
+              </div>
+              <div className='row'>
+                <div className='form-group col-6'>
+                  <span>So Dien Thoai</span>
+                  <input type='text' className='form-control' name="soDienThoai" value={this.state.soDienThoai} onChange={this.handleChange} />
+                </div>
+                <div className='form-group col-6'>
+                  <span>Email</span>
+                  <input type='text' className='form-control' name="email" value={this.state.email} onChange={this.handleChange} />
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-md-12'>
+                  <button type='submit' className='btn btn-success'>Submit</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
         dw
       </div>
     )
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    themSinhVien: (sinhVien) =>{
+      const action = {
+        type: 'THEM_SINH_VIEN',
+        sinhVien
+      }
+      dispatch(action)
+    }
+  }
+}
+
+export default connect(null,mapDispatchToProps) (FormSinhVien)
--- react-form/src/redux/reducers/QuanLySinhVienReducer.jsx
+++ react-form/src/redux/reducers/QuanLySinhVienReducer.jsx
@@ -3,5 +3,17 @@ const stateDefault = {
 }
 
 export const QuanLySinhVienReducer = (state = stateDefault, action) =>{
-    return {...state}
+
+    switch(action.type){
+        case 'THEM_SINH_VIEN':{
+            const mangSVUpdate = [...state.mangSinhVien,action.sinhVien];
+            state.mangSinhVien = mangSVUpdate;
+            return {...state}
+            
+        };break;
+        default:{
+            return {...state}
+        }
+    }
+    
 }
